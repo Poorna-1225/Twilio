5426d86af2050d0685fbfe3321af67ef136c2efd
Poorna-1225 <nagasaikumarjampni1998@gmail.com>
2024-12-17 19:50:09 -0500

    source code updated

---
+++ make_call.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from twilio.rest import Client
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+    url = "http://demo.twilio.com/docs/voice.xml",
+    to = os.getenv("MY_PHONE_NUM"),
+    from_ = os.getenv("TWILIO_PHONE_NUM"),
+)
+
+print(call.sid)
